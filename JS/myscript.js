4b934d49f663a961b49848a2268fe7291b0eb371
matyaskomuves <mkomuves714@gmail.com>
2025-01-31 14:01:26 +0100

    Added footer // updated CSS // cut JS content

--- JS/myscript.js
+++ JS/myscript.js
@@ -26,14 +26,13 @@ toggleButton.addEventListener('click', () => {
 function addNewTask() {
     const inputContainer = document.getElementById('new-task-field');
 
-    // Prevent multiple input fields
+    // Preventing multiple input fields
     if (inputContainer.querySelector('.task-input')) {
         const taskModal = new bootstrap.Modal(document.getElementById('task-modal'));
         taskModal.show();
         return;
     }
 
-    // Create the task input field
     const taskInput = document.createElement('input');
     taskInput.setAttribute('type', 'text');
     taskInput.setAttribute('placeholder', 'Enter a new task...');
@@ -41,48 +40,41 @@ function addNewTask() {
     taskInput.classList.add('task-input', 'form-control', 'w-50');
     inputContainer.appendChild(taskInput);
 
-    // Create the "Add Task" button
     const addButton = document.createElement('button');
     addButton.innerHTML = '<i class="fa-solid fa-plus"></i>';
     addButton.classList.add('btn', 'btn-primary');
     addButton.setAttribute('id', 'add-new-task-btn');
 
-    // Attach event listener for adding a task
     addButton.addEventListener('click', appendNewTask);
 
     inputContainer.appendChild(addButton);
 }
 
 function appendNewTask() {
-    const taskInput = document.querySelector('.task-input'); // Correct scope
+    const taskInput = document.querySelector('.task-input');
     const tasksContainer = document.getElementById('tasks-container');
 
     if (taskInput && taskInput.value.trim()) {
-        // Create a task card
         const taskItem = document.createElement('div');
         taskItem.classList.add('card', 'task-card', 'mb-2', 'p-2', 'd-flex', 'align-items-center', 'justify-content-center');
 
-        // Add task text
+
         const taskText = document.createElement('p');
         taskText.textContent = taskInput.value.trim();
         taskText.classList.add('mb-5');
         taskItem.appendChild(taskText);
 
-        // Add completed button
         const completedButton = document.createElement('button');
         completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
         completedButton.classList.add('btn', 'btn-success',);
         taskItem.appendChild(completedButton);
 
-        // Completed task functionality
         completedButton.addEventListener('click', () => {
             taskItem.classList.add('completed');
         });
 
-        // Append the task to the container
         tasksContainer.appendChild(taskItem);
 
-        // Clear the input and remove input/button
         taskInput.value = '';
         taskInput.parentElement.removeChild(taskInput);
         const addButton = document.getElementById('add-new-task-btn');
